9c2580918d1fa5c9848f22a7cedb4db96d86523d
Asadisnotavailable <145096006+Asadisnotavailable@users.noreply.github.com>
2024-04-30 22:58:19 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,15 @@
+
+#code
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ToDoListGui().setVisible(true);
+            }
+        });
+    }
+}
